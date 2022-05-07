8c725cbf826bb95acf75e2bb995e9df2122b5558
Alex Sherman <asherman.ca@gmail.com>
2022-05-07 16:28:36 -0700

    navbar responsiveness and menu styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,22 @@ import fish from '../assets/main-color.png';
 const Nav = () => {
   return (
     <header>
-    <nav>
-          <img className="logo" src={fish} />
-          <div className="nav-items">
-            <span>Networks</span>
-            <span>How It Works</span>
-            <span>Team</span>
-            <span>Jobs</span>
-            <span>More</span>
-            <span>English</span>
-          </div>
-        </nav>
-        </header>
+      <nav>
+        <img className="logo" src={fish} />
+        <div className="nav-items">
+          <span>Networks</span>
+          <span>How It Works</span>
+          <span>Team</span>
+          <span>Jobs</span>
+          <span>More</span>
+          <span>English</span>
+        </div>
+        <div className="nav-menu">
+          <span />
+          <span />
+        </div>
+      </nav>
+    </header>
   )
 }
 
