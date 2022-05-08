3b872f6712fee8a88a0cb339366c65b30734981f
Alex Sherman <asherman.ca@gmail.com>
2022-05-08 16:43:55 -0700

    ExchangeItem html containers and styling

--- src/components/ExchangeItem.jsx
+++ src/components/ExchangeItem.jsx
@@ -1,12 +1,30 @@
 import { Link } from 'react-router-dom'
 
 const ExchangeItem = ({exchange}) => {
-  const { id } = exchange
-  console.log(exchange)
+  const { id, name, country, trust_score_rank } = exchange
+  console.log('name', exchange)
   return (
     <div className="exchangeItem">
       <div className="exchangeItemContent">
-      <Link to={`/${id}`}>{id}</Link>
+        <div className="exchangeItemTitle">
+          {exchange.id !== 'kraken' ? <a className="exchangeItemName" href={exchange.url}>{name}</a> :
+          <span className="exchangeItemName">{name}</span>}
+          <span className="exchangeItemCountry">{country}</span>
+        </div>
+
+        <img src={exchange.image} alt={name}></img>
+
+        <div className="exchangeItemDetails">
+          <span>Trust Score Rank</span>
+          <span>{trust_score_rank}</span>
+        </div>
+
+        <Link className="exchangeItemButton" to={`/${id}`}>
+          <span>View</span>
+          <span>
+            {`>`}
+          </span>
+        </Link>
       </div>
     </div>
   )
